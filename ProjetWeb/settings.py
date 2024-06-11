409323b2401bd9aec25d528039f1caf9c3b878c0
Petrarque Loko <lokopetrarque2003@gmail.com>
2024-06-11 13:16:10 +0200

    path construction3 and static files configuration

--- ProjetWeb/settings.py
+++ ProjetWeb/settings.py
@@ -10,14 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
-# Media files (Uploads)
-# Configurations pour les fichiers médias
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Secret Key
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
